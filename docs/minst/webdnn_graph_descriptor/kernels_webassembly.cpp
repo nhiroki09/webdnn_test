f4d2dba921cd72a82255154a3b6d78f84e75bb88
nhiroki09 <nhiroki09@gmail.com>
2018-11-09 06:39:14 +0900

    modified reference path

---
+++ docs/minst/webdnn_graph_descriptor/kernels_webassembly.cpp
@@ -0,0 +1,258 @@
+
+#include <stdlib.h>
+#include <math.h>
+
+float static_buffer[149864];
+float* dynamic_buffer = nullptr;
+
+int meta_buf_0[] = {149038,135242,1,1,28,28,26,26,3,3,1,1,1,1,0,0};
+int meta_buf_1[] = {135242,0,141326,676,8,9};
+int meta_buf_2[] = {72,141326,141326,8,8,8,676};
+int meta_buf_3[] = {141326,75338,1,8,26,26,24,24,3,3,1,1,1,1,0,0};
+int meta_buf_4[] = {75338,80,116810,576,16,72};
+int meta_buf_5[] = {1232,116810,126026,16,16,16,576};
+int meta_buf_6[] = {126026,146734,1,24,24,16,12,12,2,2,2,2,0,0};
+int meta_buf_7[] = {146734,1248,149822,1,32,2304};
+int meta_buf_8[] = {74976,149822,149822,32};
+int meta_buf_9[] = {149822,75008,149854,1,10,32};
+int meta_buf_10[] = {75328,149854,149854,10};
+int meta_buf_11[] = {149854,149854,1,10};
+int* meta_buffers[] = {meta_buf_0,meta_buf_1,meta_buf_2,meta_buf_3,meta_buf_4,meta_buf_5,meta_buf_6,meta_buf_7,meta_buf_8,meta_buf_9,meta_buf_10,meta_buf_11};
+
+extern "C" void init() {
+    //static_buffer = (float*)malloc(149864 * sizeof(float));
+}
+
+extern "C" float* get_static_buffer(void) {
+    return static_buffer;
+}
+
+extern "C" float* allocate_dynamic_buffer(int count) {
+    if (dynamic_buffer) {
+        free(dynamic_buffer);
+        dynamic_buffer = nullptr;
+    }
+    dynamic_buffer = (float*)malloc(count * sizeof(float));
+    return dynamic_buffer;
+}
+
+extern "C" float* get_dynamic_buffer(void) {
+    return dynamic_buffer;
+}
+extern "C" void set_placeholder_value(int kernel_order, int offset, int value) {
+    meta_buffers[kernel_order][offset] = value;
+}
+
+void im2col_54f86552263d1a348fd988a8122143d1a6ec1c1aa7867ba5833442fb(const int * meta_buffer)
+{
+    const float *im = (static_buffer + meta_buffer[0]);
+    float *col = (static_buffer + meta_buffer[1]);
+
+    const int N = meta_buffer[2];
+    const int C1 = meta_buffer[3];
+    const int H1 = meta_buffer[4];
+    const int W1 = meta_buffer[5];
+    const int H2 = meta_buffer[6];
+    const int W2 = meta_buffer[7];
+    const int KH = meta_buffer[8];
+    const int KW = meta_buffer[9];
+    const int DH = meta_buffer[10];
+    const int DW = meta_buffer[11];
+    const int SH = meta_buffer[12];
+    const int SW = meta_buffer[13];
+    const int PH = meta_buffer[14];
+    const int PW = meta_buffer[15];
+
+    for (int gid = 0; gid < N*H2*W2*KH*KW*C1; gid += 1) {
+        const int c1 = gid % C1;
+        const int kw = gid / C1 % KW;
+        const int kh = gid / C1 / KW % KH;
+        const int w2 = gid / C1 / KW / KH % W2;
+        const int h2 = gid / C1 / KW / KH / W2 % H2;
+        const int  n = gid / C1 / KW / KH / W2 / H2;
+
+        const int h1 = h2 * SH - PH + kh * DH;
+        const int w1 = w2 * SW - PW + kw * DW;
+
+        col[gid] = (h1 < 0 || h1 >= H1 || w1 < 0 || w1 >= W1) ? 0 : im[((n*H1+h1)*W1+w1)*C1+c1];
+    }
+}
+
+
+#ifndef INCLUDE_EIGEN
+#define INCLUDE_EIGEN
+#include <Eigen/Dense>
+#endif
+
+void tensordot_b0c87b9ad9c24d5c638538a68fd9e1ad2b472c70c0e98883fad5c6af(const int * meta_buffer)
+{
+    float *A = (static_buffer + meta_buffer[0]);
+    float *B = (static_buffer + meta_buffer[1]);
+    float *C = (static_buffer + meta_buffer[2]);
+
+    Eigen::Map<Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> > a_mat(A, meta_buffer[3], meta_buffer[5]);
+    Eigen::Map<Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic, Eigen::ColMajor> > b_mat(B, meta_buffer[5], meta_buffer[4]);
+    Eigen::Map<Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> > c_mat(C, meta_buffer[3], meta_buffer[4]);
+
+    c_mat.noalias() = a_mat * b_mat;
+}
+
+
+void fusedelementwise_39f491a753592bd6d9a83c6f76917012b5eb3c0ac597e095aa174319(const int * meta_buffer)
+{
+    const float * v1 = (static_buffer + meta_buffer[0]);
+    const float * v2 = (static_buffer + meta_buffer[1]);
+    float * v3 = (static_buffer + meta_buffer[2]);
+    const int v4 = meta_buffer[3];
+    const int v5 = meta_buffer[4];
+    const int D0 = meta_buffer[5];
+    const int D1 = meta_buffer[6];
+    int d0;
+    for (d0 = ((1 > 8) ? (0 % (1 / 8)) : 0); d0 < D0; d0 += ((1 > 8) ? (1 / 8) : 1)) {
+        const float v6 = v1[d0];
+        int d1;
+        for (d1 = ((1 > 8) ? (0 / (1 / 8)) : 0); d1 < D1; d1 += ((1 > 8) ? 8 : 1)) {
+            const float v7 = v2[d0 + d1*v4];
+            float v8;
+            {
+                v8 = v7 + v6;
+            }
+            float v9;
+            {
+                v9 = v8 > 0 ? v8 : 0;
+            }
+            v3[d0 + d1*v5] = v9;
+        }
+    }
+}
+
+
+void maxpooling2d_386ebef75ea973afd833e12e5ea8f6c97456944cd3812afb94b9bb4e(const int * meta_buffer)
+{
+    const float *X = (static_buffer + meta_buffer[0]);
+    float *Y = (static_buffer + meta_buffer[1]);
+    const int N = meta_buffer[2];
+    const int H1 = meta_buffer[3];
+    const int W1 = meta_buffer[4];
+    const int C = meta_buffer[5];
+    const int H2 = meta_buffer[6];
+    const int W2 = meta_buffer[7];
+    const int KH = meta_buffer[8];
+    const int KW = meta_buffer[9];
+    const int SH = meta_buffer[10];
+    const int SW = meta_buffer[11];
+    const int PH = meta_buffer[12];
+    const int PW = meta_buffer[13];
+
+    for (int gid = 0; gid < N * H2 * W2 * C; gid += 1) {
+        const int c = gid % C;
+        const int w2 = gid / C % W2;
+        const int h2 = gid / C / W2 % H2;
+        const int n = gid / C / W2 / H2;
+
+        float v = -1e7;
+        for (int kh = 0; kh < KH; kh++) {
+            const int h1 = h2 * SH - PH + kh;
+            if (h1 < 0 || h1 >= H1) continue;
+
+            for (int kw = 0; kw < KW; kw++) {
+                const int w1 = w2 * SW - PW + kw;
+                if (w1 < 0 || w1 >= W1) continue;
+
+                v = v > X[((n * H1 + h1) * W1 + w1) * C + c] ? v : X[((n * H1 + h1) * W1 + w1) * C + c];
+            }
+        }
+
+        Y[gid] = v;
+    }
+}
+
+
+void fusedelementwise_9d5b58d4f5a00a595e84c1773b477ca77b891512fffc1c7d24d35e3d(const int * meta_buffer)
+{
+    const float * v1 = (static_buffer + meta_buffer[0]);
+    const float * v2 = (static_buffer + meta_buffer[1]);
+    float * v3 = (static_buffer + meta_buffer[2]);
+    const int D0 = meta_buffer[3];
+    int d0;
+    for (d0 = 0; d0 < D0; d0 += 1) {
+        const float v4 = v1[d0];
+        const float v5 = v2[d0];
+        float v6;
+        {
+            v6 = v5 + v4;
+        }
+        float v7;
+        {
+            v7 = v6 > 0 ? v6 : 0;
+        }
+        v3[d0] = v7;
+    }
+}
+
+
+void elementwiseadd_98450d361d3b8b06ad718e1e961bda61506f2baab3157c18aecbf897(const int * meta_buffer)
+{
+    const float * v1 = (static_buffer + meta_buffer[0]);
+    const float * v2 = (static_buffer + meta_buffer[1]);
+    float * v3 = (static_buffer + meta_buffer[2]);
+    const int D0 = meta_buffer[3];
+    int d0;
+    for (d0 = 0; d0 < D0; d0 += 1) {
+        const float v4 = v1[d0];
+        const float v5 = v2[d0];
+        float v6;
+        {
+            v6 = v5 + v4;
+        }
+        v3[d0] = v6;
+    }
+}
+
+
+void softmax_be4c2fae32b9326b0ca891226ac9234de828aecba508a30394d8a57e(const int * meta_buffer)
+{
+    const float *X = (static_buffer + meta_buffer[0]);
+    float *Y = (static_buffer + meta_buffer[1]);
+    const int N = meta_buffer[2];
+    const int C = meta_buffer[3];
+
+    for (int n = 0; n < N; n++) {
+        float set_max = X[n * C];
+        for (int c = 0; c < C; c++) {
+            float val = X[n * C + c];
+            if (val > set_max) {
+                set_max = val;
+            }
+        }
+
+        float sum_exp = 0.0F;
+        for (int c = 0; c < C; c++) {
+            float val = X[n * C + c];
+            float exp_x = expf(val - set_max);
+            sum_exp += exp_x;
+            Y[n * C + c] = exp_x;
+        }
+
+        for (int c = 0; c < C; c++) {
+            Y[n * C + c] /= sum_exp;
+        }
+    }
+}
+
+extern "C" void run() {
+im2col_54f86552263d1a348fd988a8122143d1a6ec1c1aa7867ba5833442fb(meta_buf_0);
+tensordot_b0c87b9ad9c24d5c638538a68fd9e1ad2b472c70c0e98883fad5c6af(meta_buf_1);
+fusedelementwise_39f491a753592bd6d9a83c6f76917012b5eb3c0ac597e095aa174319(meta_buf_2);
+im2col_54f86552263d1a348fd988a8122143d1a6ec1c1aa7867ba5833442fb(meta_buf_3);
+tensordot_b0c87b9ad9c24d5c638538a68fd9e1ad2b472c70c0e98883fad5c6af(meta_buf_4);
+fusedelementwise_39f491a753592bd6d9a83c6f76917012b5eb3c0ac597e095aa174319(meta_buf_5);
+maxpooling2d_386ebef75ea973afd833e12e5ea8f6c97456944cd3812afb94b9bb4e(meta_buf_6);
+tensordot_b0c87b9ad9c24d5c638538a68fd9e1ad2b472c70c0e98883fad5c6af(meta_buf_7);
+fusedelementwise_9d5b58d4f5a00a595e84c1773b477ca77b891512fffc1c7d24d35e3d(meta_buf_8);
+tensordot_b0c87b9ad9c24d5c638538a68fd9e1ad2b472c70c0e98883fad5c6af(meta_buf_9);
+elementwiseadd_98450d361d3b8b06ad718e1e961bda61506f2baab3157c18aecbf897(meta_buf_10);
+softmax_be4c2fae32b9326b0ca891226ac9234de828aecba508a30394d8a57e(meta_buf_11);
+
+}
+
